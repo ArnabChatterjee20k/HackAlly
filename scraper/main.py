423be74ce219cc78848b2ba6866bf3e10668e71f
arnab <arnabchatterjee.ac.2@gmail.com>
2024-09-09 15:01:28 +0530

    added db connection and insertion

--- scraper/main.py
+++ scraper/main.py
@@ -1,8 +1,12 @@
 from modules.devpost.devpost import scrape_devpost
 from modules.db.db import get_db_and_collection,batch_insert_with_retry
-import asyncio
+import asyncio,os
 async def main():
 
     # asyncio.get_event_loop().run_until_complete(scrape_devpost())
     data = await asyncio.gather(*[scrape_devpost()])
+    conn = os.environ.get("MONGO_URI")
+    db,col = get_db_and_collection(conn,"hackathons","events")
+    for doc in data:
+        batch_insert_with_retry(col,doc)
 asyncio.get_event_loop().run_until_complete(main())
